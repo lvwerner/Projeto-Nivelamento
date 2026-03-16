27e1c234307365af53d1358d1c4150f2fd0088c3
Vini Werner <118356029+lvwerner@users.noreply.github.com>
2026-03-16 14:37:46 -0300

    Adicionado o Formulário de solicitação de coleta de móveis Podendo descrever o móvel e enviar fotos para melhor organização da equipe de coleta.

--- script.js
+++ script.js
@@ -6,3 +6,73 @@ e.preventDefault()
 alert("Denúncia enviada!")
 
 })
+const modal = document.getElementById("modalColeta")
+const abrir = document.getElementById("abrirColeta")
+const fechar = document.querySelector(".fechar")
+
+abrir.onclick = () => modal.style.display = "block"
+
+fechar.onclick = () => modal.style.display = "none"
+
+window.onclick = (e)=>{
+if(e.target == modal){
+modal.style.display = "none"
+}
+}
+document.getElementById("formColeta")
+.addEventListener("submit", function(e){
+
+e.preventDefault()
+
+let dados = {
+nome:this[0].value,
+endereco:this[1].value,
+bairro:this[2].value,
+telefone:this[3].value,
+tipo:this[4].value,
+descricao:this[5].value
+}
+
+let coletas = JSON.parse(localStorage.getItem("coletas")) || []
+
+coletas.push(dados)
+
+localStorage.setItem("coletas", JSON.stringify(coletas))
+
+alert("Solicitação enviada!")
+
+modal.style.display="none"
+
+this.reset()
+
+})
+document.getElementById("formColeta")
+.addEventListener("submit", function(e){
+
+e.preventDefault()
+
+let foto = document.getElementById("fotoMovel").files[0]
+
+let dados = {
+nome:this[0].value,
+endereco:this[1].value,
+bairro:this[2].value,
+telefone:this[3].value,
+tipo:this[4].value,
+descricao:this[6].value,
+foto: foto ? foto.name : "sem imagem"
+}
+
+let coletas = JSON.parse(localStorage.getItem("coletas")) || []
+
+coletas.push(dados)
+
+localStorage.setItem("coletas", JSON.stringify(coletas))
+
+alert("Solicitação enviada!")
+
+modal.style.display="none"
+
+this.reset()
+
+})
